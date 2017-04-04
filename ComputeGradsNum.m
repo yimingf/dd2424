aa7eabb41d2ffee0357e422c88bad34602f99bf4
Fan Yiming <alkalidic@gmail.com>
2017-04-04 14:08:40 +0200

    all necessary files

---
+++ ComputeGradsNum.m
@@ -0,0 +1,25 @@
+function [grad_b, grad_W] = ComputeGradsNum(X, Y, W, b, lambda, h)
+
+no = size(W, 1);
+d = size(X, 1);
+
+grad_W = zeros(size(W));
+grad_b = zeros(no, 1);
+
+c = ComputeCost(X, Y, W, b, lambda);
+
+for i=1:length(b)
+    b_try = b;
+    b_try(i) = b_try(i) + h;
+    c2 = ComputeCost(X, Y, W, b_try, lambda);
+    grad_b(i) = (c2-c) / h;
+end
+
+for i=1:numel(W)   
+    
+    W_try = W;
+    W_try(i) = W_try(i) + h;
+    c2 = ComputeCost(X, Y, W_try, b, lambda);
+    
+    grad_W(i) = (c2-c) / h;
+end
---
+++ ComputeGradsNumSlow.m
@@ -0,0 +1,31 @@
+function [grad_b, grad_W] = ComputeGradsNumSlow(X, Y, W, b, lambda, h)
+
+no = size(W, 1);
+d = size(X, 1);
+
+grad_W = zeros(size(W));
+grad_b = zeros(no, 1);
+
+for i=1:length(b)
+    b_try = b;
+    b_try(i) = b_try(i) - h;
+    c1 = ComputeCost(X, Y, W, b_try, lambda);
+    b_try = b;
+    b_try(i) = b_try(i) + h;
+    c2 = ComputeCost(X, Y, W, b_try, lambda);
+    grad_b(i) = (c2-c1) / (2*h);
+end
+
+for i=1:numel(W)
+    
+    W_try = W;
+    W_try(i) = W_try(i) - h;
+    c1 = ComputeCost(X, Y, W_try, b, lambda);
+    
+    W_try = W;
+    W_try(i) = W_try(i) + h;
+    c2 = ComputeCost(X, Y, W_try, b, lambda);
+    
+    grad_W(i) = (c2-c1) / (2*h);
+end
+

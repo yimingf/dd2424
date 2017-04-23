adf02c72b897c6198841553bddd0f18afe90230f
Fan Yiming <alkalidic@gmail.com>
2017-04-23 14:42:30 +0200

    well a2

--- a2.m
+++ a2.m
@@ -23,12 +23,12 @@ end
 
 % generate the hyper-parameters.
 hyper_parameters.a          = 0.001; % variance
-hyper_parameters.eta        = 0.02; % learning rate
+hyper_parameters.eta        = 0.0028; % learning rate
 hyper_parameters.lambda     = 0.0015; % regularization rate
 hyper_parameters.n_batch    = 500; % number of batches
 hyper_parameters.n_epochs   = 60; % number of epoches
 hyper_parameters.decay_rate = 0.95;
-hyper_parameters.m          = 100; % hidden layer.
+hyper_parameters.m          = 300; % hidden layer.
 hyper_parameters.rho        = 0.9; % momentum parameter.
 
 W1 = hyper_parameters.a.*randn(hyper_parameters.m, hyper_parameters.d);
